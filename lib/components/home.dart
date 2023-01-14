d62718d83afc3a129da7e08cfd54e7c6e6090fd8
MyoThuHtay <61018567+MyoThuHtay@users.noreply.github.com>
2023-01-14 21:01:37 +0630

    split server

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -11,6 +11,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int page = 0;
+  double buttomBarWidth = 42;
+
+  onTap(int index) {
+    setState(() {
+      page = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
@@ -23,6 +32,64 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: page,
+        selectedItemColor: Colors.blueAccent,
+        backgroundColor: Colors.black45,
+        iconSize: 28,
+        onTap: onTap,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+              icon: Container(
+                width: buttomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: page == 0 ? Colors.blueAccent : Colors.transparent,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.home_outlined,
+                ),
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: buttomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: page == 1 ? Colors.blueAccent : Colors.transparent,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.shopping_cart_outlined,
+                ),
+              ),
+              label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: buttomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: page == 2 ? Colors.blueAccent : Colors.transparent,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.account_circle_outlined,
+                ),
+              ),
+              label: 'Account'),
+        ],
       ),
     );
   }
